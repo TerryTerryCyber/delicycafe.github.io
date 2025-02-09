7abefc3c4fb3b6de6fe68df1bf4269d15b4416ee
TerryTerryCyber <terry.tobey@icloud.com>
2025-02-09 07:18:27 -0800

    Working on the site. Added gallery and services page. Removed about, base, and home pages.

--- flask-template/app.py
+++ flask-template/app.py
@@ -1,13 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template # type: ignore
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/")
+@app.route('/home')
 def home():
-    return render_template("home.html")
+    return render_template('home.html')
 
-
-@app.route("/about")
+@app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template('about.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+if __name__ == '__main__':
+    app.run(debug=True) # type: ignore
